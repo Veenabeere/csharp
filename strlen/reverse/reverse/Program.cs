b100fd4c5ce8a18579c5c1e2e5b4e499a3d58351
Sahithi Veena <sahithiv@infinite.com>
2023-09-08 19:26:14 +0530

    my assessment

---
+++ strlen/reverse/reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "veena";
+            string reverse = "";
+            for (int i = 4; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+        }
+    }
+}
