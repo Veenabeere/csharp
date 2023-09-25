cdc1bd646b45bbbb45fe1f145a9d67486af4b224
Veenabeere <144128050+Veenabeere@users.noreply.github.com>
2023-09-26 00:09:33 +0530

    Add files via upload

---
+++ Assignment 7/RemotingClient/RemotingClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using RemotingServer;
+namespace RemotingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpChannel tcpChannel = new TcpChannel(8002);
+            ChannelServices.RegisterChannel(tcpChannel);
+
+            Service service = (Service)Activator.GetObject(typeof(Service),
+                "tcp://localhost:8089/OurFirstRemoteService");
+       
+            Console.WriteLine(service.SayHello("  Remote"));
+            Console.WriteLine(service.HighestNumber(20, 25));
+            Console.Read();
+        }
+    }
+}
