843d83493758aa3d806cdcfe97dded72dfef7ed6
Sahithi Veena <sahithiv@infinite.com>
2023-10-03 10:00:40 +0530

    My Task

---
+++ Assignments/Assignment 7/Remoteclient/Remoteclient/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Remoteclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.lblnuml2 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.btnhighestnum = new System.Windows.Forms.Button();
+            this.txtresult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Location = new System.Drawing.Point(319, 170);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(61, 13);
+            this.lblnum1.TabIndex = 0;
+            this.lblnum1.Text = "Enter num1";
+            // 
+            // lblnuml2
+            // 
+            this.lblnuml2.AutoSize = true;
+            this.lblnuml2.Location = new System.Drawing.Point(319, 241);
+            this.lblnuml2.Name = "lblnuml2";
+            this.lblnuml2.Size = new System.Drawing.Size(64, 13);
+            this.lblnuml2.TabIndex = 1;
+            this.lblnuml2.Text = "Enetr num 2";
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(460, 167);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 20);
+            this.txtnum1.TabIndex = 2;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(460, 234);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 20);
+            this.txtnum2.TabIndex = 3;
+            // 
+            // btnhighestnum
+            // 
+            this.btnhighestnum.Location = new System.Drawing.Point(410, 314);
+            this.btnhighestnum.Name = "btnhighestnum";
+            this.btnhighestnum.Size = new System.Drawing.Size(150, 23);
+            this.btnhighestnum.TabIndex = 4;
+            this.btnhighestnum.Text = "Highestnumber";
+            this.btnhighestnum.UseVisualStyleBackColor = true;
+            this.btnhighestnum.Click += new System.EventHandler(this.btnhighestnum_Click);
+            // 
+            // txtresult
+            // 
+            this.txtresult.Location = new System.Drawing.Point(593, 198);
+            this.txtresult.Name = "txtresult";
+            this.txtresult.Size = new System.Drawing.Size(155, 20);
+            this.txtresult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtresult);
+            this.Controls.Add(this.btnhighestnum);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.lblnuml2);
+            this.Controls.Add(this.lblnum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.Label lblnuml2;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Button btnhighestnum;
+        private System.Windows.Forms.TextBox txtresult;
+    }
+}
+
---
+++ Codebasetests/Codebasetest2/Test2(2).cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    //Create a Class called Products with
+    //Productid, Product Name, Price. Accept 10 Products, sort them based on the price, and display the sorted Products
+    class Product
+    {
+       public int ProductId;
+      public  string ProductName;
+      public  decimal Price;
+    }
+    class Test2_2_
+    {
+
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[10];
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i + 1}:");
+                Console.Write("Product ID: ");
+                
+                int productId = int.Parse(Console.ReadLine());
+                Console.Write("Product Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Price: ");
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                products[i] = new Product
+                {
+                    ProductId = productId,
+                    ProductName = productName,
+                    Price = price
+                };
+            }
+            SortProductsByPrice(products);
+            Console.WriteLine("\nSorted Products by Price:");
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}");
+                Console.WriteLine($"Product Name: {product.ProductName}");
+                Console.WriteLine($"Price: {product.Price:C2}");
+                Console.WriteLine();
+            }
+        }
+
+        static void SortProductsByPrice(Product[] products)
+        {
+            for (int i = 0; i < products.Length - 1; i++)
+            {
+                for (int j = i + 1; j < products.Length; j++)
+                {
+                    if (products[j].Price < products[i].Price)
+                    {
+                        Product temp = products[i];
+                        products[i] = products[j];
+                        products[j] = temp;
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+}
+    
