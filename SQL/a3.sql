5c2cea5d463ed7cd16707c26f8b95ea7ce415dd8
Sahithi Veena <sahithiv@infinite.com>
2023-10-11 21:50:18 +0530

    assignment 3

---
+++ SQL/a3.sql
@@ -0,0 +1,85 @@
+
+
+
+--1. Retrieve a list of MANAGERS. 
+select ename,job from emp where job='Manager'
+
+
+--2. Find out the names and salaries of all employees earning more than 1000 per 
+--month. 
+select ename,sal from emp where sal>1000
+
+
+--3. Display the names and salaries of all employees except JAMES. 
+select ename,sal from emp where ename <> 'james'
+
+
+--4. Find out the details of employees whose names begin with ‘S’. 
+select *from emp where ename like's%'
+
+
+--5. Find out the names of all employees that have ‘A’ anywhere in their name. 
+select ename from emp where ename like'%a%'
+
+
+--6. Find out the names of all employees that have ‘L’ as their third character in 
+--their name. 
+select ename from emp where ename like '__l%'
+
+
+--7. Compute daily salary of JONES. 
+select sal/30 from emp where ename='jones'
+
+
+--8. Calculate the total monthly salary of all employees.
+select sum(sal) as 'Total monthly salary' from emp
+
+
+--9. Print the average annual salary . 
+select avg(sal*12) as'avg annual salary' from emp
+
+
+--10. Select the name, job, salary, department number of all employees except 
+--SALESMAN from department number 30. 
+select ename , job,sal,deptno from emp where job not like 'salesman' and  not deptno=30 
+
+
+--11. List unique departments of the EMP table.
+select distinct dname from emp join dept on emp.deptno=dept.deptno 
+
+
+--12. List the name and salary of employees who earn more than 1500 and are 
+--in department 10 or 30. Label the columns Employee and Monthly Salary respectively.
+select ename,sal from emp where sal>1500 and (deptno=10 or deptno=30)
+
+
+--13. Display the name, job, and salary of all the employees whose job is MANAGER or 
+--ANALYST and their salary is not equal to 1000, 3000, or 5000. 
+select ename,job ,sal from emp where job in ('MANAGER','ANALYST') and sal not in (1000,3000,5000)
+
+
+--14. Display the name, salary and commission for all employees whose commission 
+--amount is greater than their salary increased by 10%. 
+select ename ,sal*0.1 as 'inc salary' ,comm from emp where comm > sal--(select sal from emp where sal=sal*0.1)
+
+
+--15. Display the name of all employees who have two Ls in their name and are in 
+--department 30 or their manager is 7782. 
+select ename from emp where ename like '%ll%' and (deptno=30 or mgr_id=7789)
+
+
+--16. Display the names of employees with experience of over 30 years and under 40 yrs.
+ --Count the total number of employees. 
+ select ename  from emp where DATEDIFF(YEAR, hiredate, GETDATE()) >= 30 
+ and DATEDIFF(YEAR, hiredate, GETDATE()) < 40
+select  count(*) as 'count' from emp where  DATEDIFF(YEAR, hiredate, GETDATE()) >= 30
+  and DATEDIFF(YEAR, hiredate, GETDATE()) < 40
+
+
+--17. Retrieve the names of departments in ascending order and their employees in descending order
+select e.ename,d.dname from dept d join emp e on d.deptno=e.deptno order by(dname) , ename desc
+
+--18. Find out experience of MILLER. 
+select DATEDIFF(year,hiredate,getdate()) as 'Millers experience' from emp where ename='miller'
+
+
